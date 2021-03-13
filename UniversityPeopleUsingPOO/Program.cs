031b8e6872155957b2c42f864a049ce587f61e68
Ille Dragos <illedragos@yahoo.com>
2021-03-13 18:50:01 +0200

    commit1

--- UniversityPeopleUsingPOO/Program.cs
+++ UniversityPeopleUsingPOO/Program.cs
@@ -14,9 +14,10 @@ namespace UniversityPeopleUsingPOO
             Console.WriteLine("**********************");
             Teacher t1 = new Teacher();
             t1.sleep();
-           //t1.eat();             //????
-
-            IActions action = t1; //????
+           //t1.eat();             //de ce nu merge? din moment ce nu merge de ce nu primesc eroare ca nu ii implementata
+                                   //metoda in clasa Teacher din moment ce implementeaza IAction                                         
+                
+            IActions action = t1; // ce obiect este action? teacher?
             action.eat();
 
             Console.WriteLine("**********************");
